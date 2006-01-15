47870a8a0d51f9429c76ad1328f5cd8d47ec545d
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2006-01-15 05:04:43 +0000

    Move the definition of the UDP port as out as possible.
    darcs-hash:20060115050443-71b0c-c86ed2c142866052f751e715daa81f097fe75239.gz

--- src/echo_sup.erl
+++ src/echo_sup.erl
@@ -1,12 +1,12 @@
 -module(echo_sup).
 -behaviour(supervisor).
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(UDPPort) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, {UDPPort}).
 
-init(_Args) ->
+init({UDPPort}) ->
     {ok, {{one_for_one, 1, 60},
           [%{echo_tcp, {echo_tcp, start_link, [10007]}, permanent, 1000, worker, [echo_tcp]},
-	   {echo_udp, {echo_udp, start_link, []}, permanent, 1000, worker, [echo_udp]}]}}.
+	   {echo_udp, {echo_udp, start_link, [UDPPort]}, permanent, 1000, worker, [echo_udp]}]}}.
--- src/echo_udp.erl
+++ src/echo_udp.erl
@@ -1,17 +1,17 @@
 -module(echo_udp).
 -behaviour(gen_server).
--export([start_link/0, stop/0]).
+-export([start_link/1, stop/0]).
 -export([init/1, handle_call/3,  handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(Port) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Port, []).
 
 stop() ->
     gen_server:cast(?MODULE, stop).
 
 %% Callbacks.
-init(_Args) ->
-    {ok, Socket} = gen_udp:open(10007, [{active, once}]), % Open the udp socket.
+init(Port) ->
+    {ok, Socket} = gen_udp:open(Port, [{active, once}]), % Open the udp socket.
     {ok, _} = echo:start_link({local, udp_echo}),   % One worker to take care of UDP.
     {ok, {Socket}}.
     
