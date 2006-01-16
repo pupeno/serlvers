8f362dc5392b252975b54a5f57ae6cc7e716509d
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2006-01-16 00:26:43 +0000

    Generalize the launcher.
    darcs-hash:20060116002643-71b0c-095440073be3bd1f6b47be4bfa0ab2880233e54c.gz

--- src/echo_launcher.erl
+++ src/echo_launcher.erl
@@ -1,16 +1,16 @@
 -module(echo_launcher).
 -behaviour(gen_server).
--export([start_link/1, start_link/2, stop/1]).
+-export([start_link/3, start_link/4, stop/1]).
 -export([init/1, handle_call/3,  handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([acceptor/1]).
 
-start_link(TransportAndPort) ->
-    io:fwrite("~w:start_link(~w)~n", [?MODULE, TransportAndPort]),
-    gen_server:start_link(?MODULE, TransportAndPort, []).
+start_link(Protocol, Transport, Port) ->
+    io:fwrite("~w:start_link(~w, ~w, ~w)~n", [?MODULE, Protocol, Transport, Port]),
+    gen_server:start_link(?MODULE, {Protocol, Transport, Port}, []).
 
-start_link(SupName, TransportAndPort) ->
-    io:fwrite("~w:start_link(~w, ~w)~n", [?MODULE, SupName, TransportAndPort]),
-    gen_server:start_link(SupName, ?MODULE, TransportAndPort, []).
+start_link(SupName, Protocol, Transport, Port) ->
+    io:fwrite("~w:start_link(~w, ~w, ~w, ~w)~n", [?MODULE, SupName, Protocol, Transport, Port]),
+    gen_server:start_link(SupName, ?MODULE, {Protocol, Transport, Port}, []).
 
 stop(Process) ->
     gen_server:cast(Process, stop).
@@ -26,19 +26,19 @@ acceptor(LSocket) ->
     acceptor(LSocket).
 
 %% Callbacks.
-init({tcp, Port}) ->
+init({Protocol, tcp, Port}) ->
     io:fwrite("~w:init(~w)~n", [?MODULE, {tcp, Port}]),
     process_flag(trap_exit, true),
     {ok, LSocket} = gen_tcp:listen(Port, [{active, once}]), % Open the tcp port.
     spawn_link(?MODULE, acceptor, [LSocket]),               % Launch the acceptor.
-    {ok, {tcp, LSocket}};
-init({udp, Port}) ->
+    {ok, {Protocol, tcp, LSocket}};
+init({Protocol, udp, Port}) ->
     io:fwrite("~w:init(~w)~n", [?MODULE, {udp, Port}]),
     process_flag(trap_exit, true),
-    {ok, Socket} = gen_udp:open(Port, [{active, once}]),   % Open the udp socket.
-    {ok, Pid} = echo:start_link({local, echo_udp_worker}), % One worker to take care of UDP.
-    gen_udp:controlling_process(Socket, Pid),              % Give the UDP port to the worker.
-    {ok, {udp, Socket}}.
+    {ok, Socket} = gen_udp:open(Port, [{active, once}]),              % Open the udp socket.
+    {ok, Pid} = Protocol:start_link({local, udpWorkerName(Protocol)}), % One worker to take care of UDP.
+    gen_udp:controlling_process(Socket, Pid),                         % Give the UDP port to the worker.
+    {ok, {Protocol, udp, Socket}}.
   
 handle_call(_Request, _From, State) ->
     io:fwrite("~w:handle_call(~w, ~w, ~w)~n", [?MODULE, _Request, _From, State]),
@@ -54,15 +54,19 @@ handle_info(_Info, State) ->
     io:fwrite("~w:handle_info(~w, ~w)~n", [?MODULE, _Info, State]),
     {noreply, State}.
     
-terminate(_Reason, {tcp, LSocket}) ->
-    io:fwrite("~w:terminate(~w, ~w)~n", [?MODULE, _Reason, {tcp, LSocket}]),
+terminate(_Reason, {Protocol, tcp, LSocket}) ->
+    io:fwrite("~w:terminate(~w, ~w)~n", [?MODULE, _Reason, {Protocol, tcp, LSocket}]),
     gen_tcp:close(LSocket), % Close the socket, we are done.
     ok;
-terminate(_Reason, {udp, Socket}) ->
-    io:fwrite("~w:terminate(~w, ~w)~n", [?MODULE, _Reason, {udp, Socket}]),
+terminate(_Reason, {Protocol, udp, Socket}) ->
+    io:fwrite("~w:terminate(~w, ~w)~n", [?MODULE, _Reason, {Protocol, udp, Socket}]),
     gen_udp:close(Socket),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
     io:fwrite("~w:code_change(~w, ~w, ~w)~n", [?MODULE, _OldVsn, State, _Extra]),
     {ok, State}.
+
+%% Utils.
+udpWorkerName(Protocol) ->
+     list_to_atom(string:concat(atom_to_list(Protocol), "_udp_worker")).
