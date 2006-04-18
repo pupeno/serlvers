62abf50d329f9b40c778432dfc9184eb1ada6443
José Pablo Ezequiel "Pupeno" Fernández Silva <pupeno@pupeno.com>
2006-04-18 14:47:43 +0000

    Start to code the DNS behaviour (modeled after daytime).
    darcs-hash:20060418144743-de23e-acc103cad012907165c393b785af0ab0dd68b68a.gz

--- src/gen_dns.erl
+++ src/gen_dns.erl
@@ -10,35 +10,51 @@
 
 %% @author José Pablo Ezequiel "Pupeno" Fernández Silva <pupeno@pupeno.com> [http://pupeno.com]
 %% @copyright 2006 José Pablo Ezequiel "Pupeno" Fernández Silva
-%% @doc The gen_time behaviour is used to implement Time servers according to <a href="http://www.ietf.org/rfc/rfc868.txt">RFC868</a>.
-%% <p>To make a time server make a module implementing the behaviour gen_time, that requires the following functions:</p>
+%% @doc The gen_dns behaviour is used to implement Dns servers according to <a href="http://www.ietf.org/rfc/rfc1034.txt">RFC1034</a> and <a href="http://www.ietf.org/rfc/rfc1035.txt">RFC1035</a>.
+%% <p>To make a dns server make a module implementing the behaviour gen_dns, that requires the following functions:</p>
 %% <ul>
-%%  <li>init/1: Initialization code. It gets the arguments passed to one of the start functions and it should return the state of the time server.</li>
-%%  <li>time/1: The main function. The parameter is the state of the server. The result should be a tuple of two items, the first being the string that would be sent as time, the second being the new state.</li>
-%%  <li>terminate/2: Any clean up code should be here. The first parameter is the reason to terminate, the second is the state.</li>
+%%  <li>...</li>
 %% </ul>
 %% @see launcher.
-%% @since 0.1.0
+%% @since 0.2
 
--module(gen_time).
+-module(gen_dns).
 -behaviour(gen_server).
--export([start/3, start/4, start_link/3, start_link/4, stop/1]).
--export([init/1, handle_call/3,  handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([behaviour_info/1]).
+%-export([start/3, start/4, start_link/3, start_link/4, stop/1]).
+%-export([init/1, handle_call/3,  handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+%-export([behaviour_info/1]).
 
-%% @doc Function used by Erlang (compiler?) to ensure that a module implementing gen_time really is exporting the needed functions.
+%% @doc Structure defining a DNS message. It is based on what is defined on RFC1035 <http://www.ietf.org/rfc/rfc1035.txt> but it has been re-arranged for easy of use and some fields than are not needed where removed (the counts, which can be calculated out of the length of the lists).
+%% @since 0.2
+-record(dns_message, {
+	  identifier, %% Assigned by the program that generates queries.  This identifier is copied to the corresponding reply and can be used by the requester to match up replies to outstanding queries.
+	  qr,         %% Whether this message is a query ('False'), or a response ('True').
+	  opcode,     %% Kind of query in this message.  This value is set by the originator of a query and copied into the response.
+	  aa,         %% The responding name server is an authority for the domain name in question section.
+	  tc,         %% Specifies that this message was truncated due to length greater than that permitted on the transmission channel.
+	  rd,         %% This may be set in a query and is copied into the response.  If RD is set, it directs the name server to pursue the query recursively.
+	  ra,         %% Is set or cleared in a response, and denotes whether recursive query support is available in the name server.
+	  rcode,      %% Type of the response.
+	  question,   %% The question for the name server.
+	  answer,     %% 'ResourceRecord's answering the question.
+	  authority,  %% 'ResourceRecord's pointing toward an authority.
+	  additional  %% 'ResourceRecord's holding additional information.
+	 }).
+
+
+%% @doc Function used by Erlang (compiler?) to ensure that a module implementing gen_dns really is exporting the needed functions.
 %% @private Only Erlang itself should call this function.
-%% @since 0.1.0
+%% @since 0.2
 behaviour_info(callbacks) ->
-    [{init, 1}, {time, 1}, {terminate, 2}];
+    [{init, 1}, {daytime, 1}, {terminate, 2}];
 behaviour_info(_) ->
     undefined.
 
-%% @doc Start an unnamed time server.
+%% @doc Start an unnamed dns server.
 %% <p>See {@link start_link/4} for further explanation of the parameters.</p>
 %% @see start/4
 %% @see start_link/3
-%% @since 0.1.0
+%% @since 0.2
 %% @spec (Module::atom(), Args::term(), Options) -> Result
 %%   Options = [Option]
 %%     Option = {debug, Dbgs} | {timeout, Time} | {spawn_opt, SOpts} 
@@ -50,11 +66,11 @@ start(Module, Args, Options) ->
     %%io:fwrite("~w:start(~w, ~w, ~w)~n", [?MODULE, Module, Args, Options]),
     gen_server:start(?MODULE, {Module, Args}, Options).
 
-%% @doc Start a named time server.
+%% @doc Start a named dns server.
 %% <p>See {@link start_link/4} for further explanation of the parameters.</p>
 %% @see start/3
 %% @see start_link/4
-%% @since 0.1.0
+%% @since 0.2
 %% @spec (SupName, Module::atom(), Args::term(), Options) -> Result
 %%   SupName = {local, atom()} | {global, atom()}
 %%   Options = [Option]
@@ -67,11 +83,11 @@ start(SupName, Module, Args, Options) ->
     %%io:fwrite("~w:start(~w, ~w, ~w, ~w)~n", [?MODULE, SupName, Module, Args, Options]),
     gen_server:start(SupName, ?MODULE, {Module, Args}, Options).
 
-%% @doc Start an unnamed time server and link to it.
+%% @doc Start an unnamed dns server and link to it.
 %% <p>See {@link start_link/4} for further explanation of the parameters.</p>
 %% @see start_link/4
 %% @see start/3
-%% @since 0.1.0
+%% @since 0.2
 %% @spec (Module::atom(), Args::term(), Options) -> Result
 %%   Options = [Option]
 %%     Option = {debug, Dbgs} | {timeout, Time} | {spawn_opt, SOpts} 
@@ -83,11 +99,11 @@ start_link(Module, Args, Options) ->
     %%io:fwrite("~w:start_link(~w, ~w, ~w)~n", [?MODULE, Module, Args, Options]),
     gen_server:start_link(?MODULE, {Module, Args}, Options).
 
-%% @doc Start a named time server and link to it.
+%% @doc Start a named dns server and link to it.
 %% <p>The parameters are exactly the same as gen_server and most of them (like Options) are passed as is to gen_server.</p>
 %% @see start_link/3
 %% @see start/4
-%% @since 0.1.0
+%% @since 0.2
 %% @spec (SupName, Module::atom(), Args::term(), Options) -> Result
 %%   SupName = {local, atom()} | {global, atom()}
 %%   Options = [Option]
@@ -105,31 +121,32 @@ start_link(SupName, Module, Args, Options) ->
 %% @see start/4
 %% @see start_link/3
 %% @see start_link/4
-%% @since 0.1.0
+%% @since 0.2
 %% @spec (Name) -> ok
 %%   Name = atom() | {local, atom()} | {global, atom()}
-stop(Module) ->
-    gen_server:cast(Module, stop).
+stop(Process) ->
+    %%io:fwrite("~w:stop(~w)~n", [?MODULE, Process]),
+    gen_server:cast(Process, stop).
 
-%% @doc This function gets called by gen_server to initialize the module. After some basic internal initialization the init function of the module implementing the particular time server gets called (same as this module implementing a particular gen_server).
+%% @doc This function gets called by gen_server to initialize the module. After some basic internal initialization the init function of the module implementing the particular dns server gets called (same as this module implementing a particular gen_server).
 %% @private Only gen_server should call this function.
-%% @since 0.1.0
+%% @since 0.2
 init({Module, Args}) ->
     %%io:fwrite("~w:init(~w)~n", [?MODULE, {Module, Args}]),
-    process_flag(trap_exit, true), % To get terminate/2 called when exiting.
+    process_flag(trap_exit, true),
     {ok, ModState} = Module:init(Args),
     {ok, {Module, ModState}}.
 
 %% @doc The base module, gen_server may call this function. Currently there's nothing to be done here.
 %% @private Only gen_server should call this function.
-%% @since 0.1.0
+%% @since 0.2
 handle_call(_Request, _From, State) ->
     %%io:fwrite("~w:handle_call(~w, ~w, ~w)~n", [?MODULE, _Request, _From, State]),
     {noreply, State}.
 
-%% @doc This function handles the stoping of a time server.
+%% @doc This function handles stoping the dns server.
 %% @private Only gen_server should call this function.
-%% @since 0.1.0
+%% @since 0.2
 handle_cast(stop, State) ->
     %%io:fwrite("~w:handle_cast(~w, ~w)~n", [?MODULE, stop, State]),
     {stop, normal, State};
@@ -137,17 +154,17 @@ handle_cast(_Request, State) ->
     %%io:fwrite("~w:handle_cast(~w, ~w)~n", [?MODULE, _Request, State]),
     {noreply, State}.
 
-%% @doc This function handles the udp and tcp case.
+%% @doc This function handles the udp and tcp case of dns.
 %% @private Only gen_server should call this function.
-%% @since 0.1.0
+%% @since 0.2
 handle_info({connected, Socket}, {Module, ModState}) ->
     %%io:fwrite("~w:handle_cast(~w, ~w)~n", [?MODULE, {started, Socket}, {Module, ModState}]),
-    {Reply, NewModState} = Module:time(ModState),
+    {Reply, NewModState} = Module:dns(ModState),
     gen_tcp:send(Socket, Reply),
     {stop, normal, {Module, NewModState}};
 handle_info({udp, Socket, IP, InPortNo, _Packet}, {Module, ModState}) -> % Handle UDP packages.
     %%io:fwrite("~w:handle_info(~w, ~w)~n", [?MODULE, {udp, Socket, IP, InPortNo, _Packet} , {Module, ModState}]),
-    {Reply, NewModState} = Module:time(ModState), % Generate the reply.
+    {Reply, NewModState} = Module:dns(ModState), % Generate the reply.
     gen_udp:send(Socket, IP, InPortNo, Reply),       % Send the reply.
     ok = inet:setopts(Socket, [{active, once}]),     % Enable receiving of packages, get the next one.
     {stop, normal, {Module, NewModState}};
@@ -155,9 +172,9 @@ handle_info(_Info, State) ->
     %%io:fwrite("~w:handle_info(~w, ~w)~n", [?MODULE, _Info, State]),
     {noreply, State}.
 
-%% @doc This function get's called by the underling gen_server and we just pass it over to the module implementing a time server.
-%% @private Only gen_server should call this function.  
-%% @since 0.1.0
+%% @doc This function get's called by the underling gen_server and we just pass it over to the module implementing a dns server.
+%% @private Only gen_server should call this function.
+%% @since 0.2
 terminate(Reason, {Module, ModState}) ->
     %%io:fwrite("~w:terminate(~w, ~w)~n", [?MODULE, Reason, {Module, ModState}]),
     ok = Module:terminate(Reason, ModState),
@@ -165,7 +182,7 @@ terminate(Reason, {Module, ModState}) ->
 
 %% @doc Err... &lt;sarcasm&gt;code changes ?&lt;/sarcasm&gt;
 %% @private I think no one is interested in this function, yet.
-%% @since 0.1.0
+%% @since 0.2
 code_change(_OldVsn, State, _Extra) ->
     %%io:fwrite("~w:code_change(~w, ~w, ~w)~n", [?MODULE, _OldVsn, State, _Extra]),
     {ok, State}.
